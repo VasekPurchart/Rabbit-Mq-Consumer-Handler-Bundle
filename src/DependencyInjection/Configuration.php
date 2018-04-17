3ae71bcc9f3a7b4d2beea29cb16c6ab13a15d0f9
Vasek Purchart <me@vasekpurchart.cz>
2018-04-17 18:31:06 +0200

    configure single nodes

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -20,6 +20,7 @@ class Configuration extends \Consistence\ObjectPrototype implements \Symfony\Com
 	public const PARAMETER_LOGGER_SERVICE_ID = 'service_id';
 	public const PARAMETER_STOP_CONSUMER_SLEEP_SECONDS = 'stop_consumer_sleep_seconds';
 
+	public const SECTION_CONSUMERS = 'consumers';
 	public const SECTION_ENTITY_MANAGER = 'entity_manager';
 	public const SECTION_LOGGER = 'logger';
 
@@ -45,38 +46,47 @@ class Configuration extends \Consistence\ObjectPrototype implements \Symfony\Com
 
 	private function addConsumerConfiguration(ArrayNodeDefinition $node): void
 	{
-		$node
+		$consumerSleepSecondsParameter = $node
 			->children()
-				->integerNode(self::PARAMETER_STOP_CONSUMER_SLEEP_SECONDS)
-					->info('Generally how long is needed for the program to run, to be considered started, achieved by sleeping when stopping prematurely')
-					->min(0)
-					->defaultValue(self::DEFAULT_STOP_CONSUMER_SLEEP_SECONDS)
-					->treatFalseLike(0)
-					->end()
-				->arrayNode(self::SECTION_LOGGER)
-					->addDefaultsIfNotSet()
-					->children()
-						->scalarNode(self::PARAMETER_LOGGER_SERVICE_ID)
-							->info('Logger service ID, which instance will be used to log messages and exceptions')
-							->defaultValue(self::DEFAULT_LOGGER_SERVICE_ID)
-							->end()
-						->end()
-					->end()
-				->arrayNode(self::SECTION_ENTITY_MANAGER)
-					->addDefaultsIfNotSet()
-					->children()
-						->scalarNode(self::PARAMETER_ENTITY_MANAGER_SERVICE_ID)
-							->info('EntityManager service ID, which instance is used within the consumer')
-							->defaultValue(self::DEFAULT_ENTITY_MANAGER_SERVICE_ID)
-							->end()
-						->booleanNode(self::PARAMETER_ENTITY_MANAGER_CLEAR)
-							->info('Clear EntityManager before processing message')
-							->defaultValue(true)
-							->end()
-						->end()
-					->end()
-				->end()
-			->end();
+			->integerNode(self::PARAMETER_STOP_CONSUMER_SLEEP_SECONDS);
+		$consumerSleepSecondsParameter
+			->info('Generally how long is needed for the program to run, to be considered started, achieved by sleeping when stopping prematurely')
+			->min(0)
+			->defaultValue(self::DEFAULT_STOP_CONSUMER_SLEEP_SECONDS)
+			->treatFalseLike(0);
+
+		$loggerSection = $node
+			->children()
+			->arrayNode(self::SECTION_LOGGER);
+		$loggerSection
+			->addDefaultsIfNotSet();
+
+		$loggerServiceIdParameter = $loggerSection
+			->children()
+			->scalarNode(self::PARAMETER_LOGGER_SERVICE_ID);
+		$loggerServiceIdParameter
+			->info('Logger service ID, which instance will be used to log messages and exceptions')
+			->defaultValue(self::DEFAULT_LOGGER_SERVICE_ID);
+
+		$entityManagerSection = $node
+			->children()
+			->arrayNode(self::SECTION_ENTITY_MANAGER);
+		$entityManagerSection
+			->addDefaultsIfNotSet();
+
+		$entityManagerServiceIdParameter = $entityManagerSection
+			->children()
+			->scalarNode(self::PARAMETER_ENTITY_MANAGER_SERVICE_ID);
+		$entityManagerServiceIdParameter
+			->info('EntityManager service ID, which instance is used within the consumer')
+			->defaultValue(self::DEFAULT_ENTITY_MANAGER_SERVICE_ID);
+
+		$entityManagerClearParameter = $entityManagerSection
+			->children()
+			->booleanNode(self::PARAMETER_ENTITY_MANAGER_CLEAR);
+		$entityManagerClearParameter
+			->info('Clear EntityManager before processing message')
+			->defaultValue(true);
 	}
 
 }
